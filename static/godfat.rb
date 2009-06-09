df450f7c87f8b8d083bd57c2b69962993af3c432
Lin Jen-Shin <godfat@godfat.org>
2009-06-09 23:25:13 +0800

    this is more radical

--- static/godfat.rb
+++ static/godfat.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-%w[ <<self Class ].each{ |klass|
+%w[ Class <<self Symbol ].each{ |klass|
   eval <<-RUBY.split(/godfat/).join
     class #{klass}
       godfat define_method(:method_missing, &:say)
@@ -8,7 +8,7 @@
   RUBY
 }
 Class.send(:define_method, :const_missing,
-  &lambda{ |c| say c.to_s[0..-2] })
+  &lambda{ |c| cry c.to_s[0..-2] })
 def say s = nil
   print s, ' ' if s
   self
