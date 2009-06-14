60ea5dd0ade08558d80d54c2380933dc8f1ae9a9
Lin Jen-Shin <godfat@godfat.org>
2009-06-15 01:52:04 +0800

    this help me update 0rz-index of my blogger blog

---
+++ 0rz-index.rb
@@ -0,0 +1,25 @@
+
+require 'nokogiri'
+require 'open-uri'
+
+# http://www.blogger.com/feeds/4013660499115623904/posts/default/8669382733745187064
+index = Nokogiri::XML.parse(open('http://blogger.godfat.org/2007/03/index-by-date.html').read)
+current = index.css('.post-body p')[0].children.find{ |n| n.to_s =~/^(\d+)\./ }
+number, last = $1.to_i, current.next['href']
+
+atom = Nokogiri::XML.parse(open('http://blogger.godfat.org/feeds/posts/default').read)
+result = []
+atom.css('entry').each(){ |entry|
+  href = entry.css('link').find{ |link|
+                             link['rel']  == 'alternate' &&
+                             link['type'] == 'text/html'
+                           }['href'].sub(%r{.*//[^/]*}, '')
+
+  break if href == last
+  result << [Time.parse(entry.css('published').inner_text).strftime('%m-%d') + ' ' +
+             entry.css('title').inner_text, href]
+}
+
+result.each.with_index{ |post, index|
+  puts "#{number + result.size - index}. <a href=\"#{post.last}\">#{post.first}</a>"
+}
