d5b346b071c13d5d474c940dd02062ac76e316d1
Lin Jen-Shin <godfat@godfat.org>
2009-06-22 18:09:19 +0800

    add full public_send implementation, add jquery to toggle compatibility

--- static/godfat.rb
+++ static/godfat.rb
@@ -3,10 +3,24 @@
 %w[ Class <<self Symbol ].reverse.each{ |klass|
   eval <<-RUBY.split(/真常/).join
     class #{klass}
-      # 1.8 compatibility
+      # dirty 1.8 compatibility
       def public_send *args
         respond_to?(args.first) && send(*args)
       end unless respond_to?(:public_send)
+
+      # full 1.8 compatibility
+      def public_send *args
+        if respond_to?(args.first)
+          send(*args)
+        else
+          msg = " `\#{args.first}' called for \#{inspect}:\#{self.class}"
+          if respond_to?(args.first, true)
+            raise NoMethodError.new(  'private method' + msg)
+          else
+            raise NoMethodError.new('undefined method' + msg)
+          end
+        end
+      end if RUBY_VERSION < '1.9.0'
 
       # rubinius, jruby compatibility
       def to_proc
