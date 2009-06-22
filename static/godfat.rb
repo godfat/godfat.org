2d4c4a24639d96d2cf9660ada079eec1db486191
Lin Jen-Shin <godfat@godfat.org>
2009-06-22 15:46:15 +0800

    1.8, rubinius, jruby compatibility update

--- static/godfat.rb
+++ static/godfat.rb
@@ -1,8 +1,19 @@
 #!/usr/bin/env ruby
 # encoding: utf-8
-%w[ Class <<self Symbol ].each{ |klass|
+%w[ Class <<self Symbol ].reverse.each{ |klass|
   eval <<-RUBY.split(/真常/).join
     class #{klass}
+      # 1.8 compatibility
+      def public_send *args
+        respond_to?(args.first) && send(*args)
+      end unless respond_to?(:public_send)
+
+      # rubinius, jruby compatibility
+      def to_proc
+        msg = self
+        lambda{ |*args| args.shift.public_send(msg, *args) }
+      end
+
       真常 define_method(:method_missing, &:say)
     end
   RUBY
