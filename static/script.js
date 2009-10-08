b95b90c47fb59b0eb8467a880cd759b5e14c0109
Lin Jen-Shin <godfat@godfat.org>
2009-10-08 16:56:22 +0800

    [index.xhtml, script.js] extracted javascript.

---
+++ static/script.js
@@ -0,0 +1,23 @@
+var code  = ['#d18', '#f18', '#rbx-jruby']
+
+var news  = ['#news-2007-08-23', '#news-2008-12-12',
+             '#news-2008-12-17', '#news-2009-06-09',
+             '#news-2009-09-05']
+
+var items = ['#websites', '#projects', '#slides',
+             '#programming-languages', '#misc', '#time-graphs']
+
+jQuery.each(
+  code.concat(news).concat(items),
+  function(i, v){
+    $(v).hide();
+    $(v + '-toggle').click( function(){ $(v).toggle(); } );
+  });
+
+function fold_all(){
+  jQuery.each( items, function(i, v){ $(v).hide() });
+}
+
+function collapse_all(){
+  jQuery.each( items, function(i, v){ $(v).show() });
+}
