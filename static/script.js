d18462d519987e872ed415a8a324090acfb7b204
Lin Jen-Shin <godfat@godfat.org>
2009-10-11 01:24:30 +0800

    [script.js] fold latest news

--- static/script.js
+++ static/script.js
@@ -2,7 +2,7 @@ var code  = ['#d18', '#f18', '#rbx-jruby']
 
 var news  = ['#news-2007-08-23', '#news-2008-12-12',
              '#news-2008-12-17', '#news-2009-06-09',
-             '#news-2009-09-05']
+             '#news-2009-09-05', '#news-2009-10-08']
 
 var items = ['#links', '#projects', '#slides',
              '#programming-languages', '#favors',
